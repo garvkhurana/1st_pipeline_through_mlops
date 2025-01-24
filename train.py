ec1e16708338a50dace193a5491fbbb90794e3a0
Garv Khurana <146458741+garvkhurana@users.noreply.github.com>
2025-01-25 01:46:37 +0530

    Update train.py

--- train.py
+++ train.py
@@ -11,9 +11,6 @@ from urllib.parse import urlparse
 
 
 
-os.environ['MLFLOW_TRACKING_URL'] = "https://dagshub.com/garvkhurana/firstmlopspipeline.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"] = "garvkhurana"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "00ffbefccd789ae963b92c71cfff3114090e5647"
 
 
 params=yaml.safe_load(open('params.yaml'))['train']
